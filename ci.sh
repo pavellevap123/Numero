6cb49f5bc50f26db65f3a7d8e604fd29f92c0e14
Pavel Paddubotski <pavel.paddubotski@gmail.com>
2024-05-08 07:06:28 +0200

    Edit ci.sh for Jenkins

--- ci.sh
+++ ci.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-xcodebuild -project 'Numero.xcodeproject' -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 15' test
-xcodebuild -project 'Numero.xcodeproject' -scheme 'Numero' -destination 'generic/platform=iOS' -configuration Release build CODE_SIGNING_ALLOWED=NO
+xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'platform=iOS Simulator,name=iPhone 15' test
+xcodebuild -project 'Numero.xcodeproj' -scheme 'Numero' -destination 'generic/platform=iOS' -configuration Release build CODE_SIGNING_ALLOWED=NO
